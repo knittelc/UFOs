b239db0682362497231a3e461917b421266d7ca3
Courtney Knittel <102183530+knittelc@users.noreply.github.com>
2022-06-15 22:56:00 -0400

    tweaking html files and css

---
+++ UFOs/static/js/app_1.js
@@ -0,0 +1,43 @@
+// import the data from data.js
+const tableData = data;
+
+// reference the HTML table using d3
+var tbody = d3.select("tbody")
+
+// clear out any existing data
+function buildTable(data) {
+    tbody.html("");
+}
+
+// loop through each object and append a row and cells for each value in the row
+data.forEach((dataRow) => {
+    // append a row to the table body
+    let row = tbody.append("tr");
+    // loop through each field in the dataRaw and add each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }   
+    );
+});
+
+function handleClick() {
+    // grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    // check to see if a date was entered and filter with that date
+    if (date) {
+        // apply 'filter' to the table data and only keep rows where 'datetime' value matches filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    //Rebild the table
+    // @NOTE: If no data was entered, then filteredData will be original tableData
+    buildTable(filteredData);
+};
+
+// attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// build table when page loads
+buildTable(tableData);
+
